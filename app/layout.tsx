f445d98b19d428caf2f1e8a4fbbc9684ed173355
Jarren San Jose <sanjose.jarren@gmail.com>
2025-05-13 21:37:10 -0700

    redesign header and include sign-in status in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,14 +30,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
-        <div className="absolute top-4 right-4 z-50">
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-        </div>
         {children}
         <Analytics />
         </body>
